457ccd390bb5c51e6e0a7b2af8dc773c75c63c6b
Mike Wilkinson <mwilkinson@imo-online.com>
2025-01-06 22:00:37 -0600

    Remove unused reference

--- react-solitaire/src/app/App.tsx
+++ react-solitaire/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import './App.css'
 import { CardProps, CardSource } from '../components/playing-card/PlayingCard.tsx'
 import { CardSuit } from '../shared/enums.ts';
