d39bb76c0496a9612b236df3610d4f453fa2bc9b
Mike Wilkinson <mwilkinson@imo-online.com>
2025-01-07 23:13:21 -0600

    Implement auto-solve button

---
+++ react-solitaire/src/components/side-panel/stopwatch/Stopwatch.tsx
@@ -0,0 +1,52 @@
+import './Stopwatch.css';
+
+import { ForwardedRef, forwardRef, useImperativeHandle, useRef, useState } from "react";
+
+export interface StopwatchHandle {
+    setPaused: (isPaused: boolean) => void,
+    reset: () => void
+}
+
+const Stopwatch = forwardRef(function Stopwatch({ },ref: ForwardedRef<unknown>) {
+    const [startTime, setStartTime] = useState(Date.now());
+    const [now, setNow] = useState(Date.now());
+    const [isRunning, setRunning] = useState(true);
+    const intervalRef = useRef<number | null>(null);
+
+    useImperativeHandle(ref, () => {
+        return {
+            setPaused(isPaused: boolean) {
+                setRunning(!isPaused);
+                if (intervalRef.current) {
+                    clearInterval(intervalRef.current);
+                }
+            },
+            reset() {
+                console.log('setting start time');
+                setStartTime(Date.now());
+            }
+        }
+    });
+
+    if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+    }
+    intervalRef.current = setInterval(() => {
+        if (isRunning) {
+            setNow(Date.now);
+        }
+    }, 250);
+
+    let totalSeconds = Math.floor((now - startTime) / 1000);
+    totalSeconds = (totalSeconds < 0) ? 0 : totalSeconds;
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    return (
+        <div className="stopwatch-box">
+            <span className="stopwatch-text">{minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</span>
+        </div>
+    )
+});
+
+export default Stopwatch;
